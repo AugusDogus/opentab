c7de099d644454b636b8015b29efa7ef1a3631ee
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2025-12-22 01:54:53 -0600

    ci(release): enable auto-push for version tags

--- bump.config.ts
+++ bump.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   files: ["apps/extension/package.json", "apps/native/package.json"],
   commit: "chore: release v%s",
   tag: "v%s",
-  push: false,
+  push: true,
 });
 
